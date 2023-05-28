f1a3a542e87d16d281715a5caee8f3a3d30fd500
Erdyroxx <erdyroxx@gmail.com>
2023-05-28 19:41:30 +0800

    Instance Object

--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02;
 
-/**
- *
- * @author heartbeat
- */
 public class RectangleAksi {
-    
+    public static void main(String[] args) {
+       Rectangle r1 = new Rectangle();
+    }
 }
